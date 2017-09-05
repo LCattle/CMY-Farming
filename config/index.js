8c201826aa4f7b7981d2a25962807d1c830a1c73
LCattle <linguohui@yeah.net>
2017-09-05 20:55:37 +0800

    add post request

--- config/index.js
+++ config/index.js
@@ -27,7 +27,16 @@ module.exports = {
     autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    // proxy api request
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:3000/page-data/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
--- src/fetch/api.js
+++ src/fetch/api.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import Fetch from './fetchPost'
+//import Fetch from './fetchPost'
 export function fetchHomePage () {
-     return Vue.axios.get('http://localhost:3000/page-data/index-all-data')
+     return Vue.axios.get('/api/index-all-data')
+     //return Vue.axios.get('http://localhost:3000/page-data/index-all-data')
 }
 
 export function fetchGetNav () {
@@ -11,6 +12,6 @@ export function fetchGetNav () {
 export function fetchGetSubNavById (idx) {
     console.log('获取子导航！');
     //return Vue.axios.get('http://localhost:3000/page-data/index-all-data?id=' + idx.id)
-    //return Vue.axios.post('http://localhost:3000/page-data/index-all-data', {'id': idx.id})
-    return Fetch.FetchPost('tpost', {'id': idx.id});
+    return Vue.axios.post('/api/test-post', {'id': idx.id})
+   // return Fetch.FetchPost('/api/test-post', {'id': idx.id});
 }
